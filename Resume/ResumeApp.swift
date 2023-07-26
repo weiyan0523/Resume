4c5b08bc4c782d852077a64cfe727078de64e7e5
Wei Yan <weiyan0523@gmail.com>
2023-07-26 13:12:35 -0500

    Initial Commit

---
+++ Resume/ResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeApp.swift
+//  Resume
+//
+//  Created by Wei Yan on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
