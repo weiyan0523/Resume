d4f283bf2e237d0f04bd40e2b3cc60a17e79327a
Wei Yan <weiyan0523@gmail.com>
2023-07-29 13:21:14 -0500

    update HomePage

--- Resume/View/HomePage.swift
+++ Resume/View/HomePage.swift
@@ -9,14 +9,34 @@ import SwiftUI
 
 struct HomePage: View {
     var body: some View {
-        VStack{
-            Image("Selfie")
-                .resizable()
-                .scaledToFit()
-                .clipShape(Circle())
-                .frame(width: 200, height: 200)
-            Text("Hello")
+        ZStack{
+            Rectangle()
+                //.foregroundColor(Color(hue: 0.224, saturation: 0.693, brightness: 0.477))
+                .fill(LinearGradient(gradient: Gradient(colors: [.green,.blue]), startPoint: .top, endPoint: .bottom))
+            VStack{
+                ZStack{
+                    Image("Background")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: .infinity)
+                    Image("Selfie")
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(Circle())
+                        .frame(width: 200, height: 200)
+                }
+                
+                ScrollView{
+                    Text("Education")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                }
+                
+            }
         }
+        .ignoresSafeArea()
+        
     }
 }
 
